3a1156e0fc369fe0ffcb41b5d6bbfc365980eb0f
Dev <dparmar1@isi-montreal.com>
2022-07-13 22:23:00 -0400

    Signup api

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+require('./db/config')
 
+const express = require('express')
+const mongoose = require('mongoose')
+const userModel = require('./db/User')
+const cors = require('cors')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('app working')
+app.use(express.json())
+app.use(cors())
+
+app.post('/register', async (req, res) => {
+    let data = new userModel(req.body);
+    let result = await data.save()
+    res.send(result)
 })
 
 app.listen(6969)
